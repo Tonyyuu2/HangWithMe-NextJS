35799afa41e2e1fd3390e2c2a9d59e462cf5d32b
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-11-03 11:50:55 -0700

    figured out getStaticProps

--- app/pages/[hangoutId]/index.js
+++ app/pages/[hangoutId]/index.js
@@ -2,10 +2,10 @@ import { MongoClient } from "mongodb";
 import React from "react";
 import HangoutDetail from "../../components/hangouts/HangoutDetail";
 
-export default function HangoutDetails() {
+export default function HangoutDetails(props) {
   return (
     <>
-      <HangoutDetail />
+      <HangoutDetail {...props}/>
     </>
   );
 }
@@ -19,16 +19,38 @@ export async function getStaticPaths() {
 
   const hangoutsCollection = db.collection("hangouts");
 
+  const hangouts = await hangoutsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
-    paths: [
-      {
-        params: {
-          hangoutId: "m1",
-        },
-        params: {
-          hangoutId: "m2",
-        },
-      },
-    ],
+    fallback: false,
+    paths: hangouts.map((hangout) => ({
+      params: { hangoutId: hangout._id.toString() },
+    })),
   };
+}
+
+export async function getStaticProps(context) {
+
+  const hangoutId = context.params.hangoutId
+
+  const client = await MongoClient.connect(
+    `mongodb+srv://tonyyuu2:${process.env.DB_PASS}@cluster0.vdjd6hv.mongodb.net/hangwithme?retryWrites=true&w=majority`
+  );
+
+  const db = client.db();
+
+  const hangoutsCollection = db.collection("hangouts");
+
+  const selectedHangout = await hangoutsCollection.findOne({_id: hangoutId });
+
+
+  client.close();
+
+  return {
+    props: {
+      hangoutData: selectedHangout,
+    }
+  }
 }
