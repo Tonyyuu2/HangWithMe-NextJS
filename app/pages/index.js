120f0296152cfabc4351a72d84e48e18f3f6b4e0
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-11-03 13:37:17 -0700

    add Head

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,39 +1,42 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
+import Head from "next/head";
 
 import HangoutList from "../components/hangouts/HangoutList";
-
-import testImage from "../public/images/testimage.jpg";
 
 export default function Home({ hangouts }) {
   return (
     <div>
+      <Head>
+        <title>EventLoops</title>
+      </Head>
       <HangoutList hangouts={hangouts} />
     </div>
   );
 }
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://tonyyuu2:${process.env.DB_PASS}@cluster0.vdjd6hv.mongodb.net/hangwithme?retryWrites=true&w=majority`
+  );
 
-  const client = await MongoClient.connect(`mongodb+srv://tonyyuu2:${process.env.DB_PASS}@cluster0.vdjd6hv.mongodb.net/hangwithme?retryWrites=true&w=majority`);
-    
-    const db = client.db();
+  const db = client.db();
 
-    const hangoutsCollection = db.collection('hangouts');
+  const hangoutsCollection = db.collection("hangouts");
 
-    const hangouts = await hangoutsCollection.find().toArray();
-    console.log('hangouts :', hangouts);
+  const hangouts = await hangoutsCollection.find().toArray();
+  console.log("hangouts :", hangouts);
 
-    client.close();
+  client.close();
 
   return {
     props: {
-      hangouts: hangouts.map(hangout => ({
+      hangouts: hangouts.map((hangout) => ({
         title: hangout.title,
         address: hangout.address,
         image: hangout.image,
-        id: hangout._id.toString()
+        id: hangout._id.toString(),
       })),
     },
-    revalidate: 10
+    revalidate: 10,
   };
 }
